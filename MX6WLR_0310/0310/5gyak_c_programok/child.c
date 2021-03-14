622f580afd5983f5520f4e5bd7b39c0a0f27eb2c
SzAlen <78854495+SzAlen@users.noreply.github.com>
2021-03-14 08:53:45 +0100

    Add files via upload

---
+++ MX6WLR_0310/0310/5gyak_c_programok/child.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+    for(int i = 0; i < 5; i++){
+        printf("SzA MX6WLR\n");
+    }
+    return 0;
+}
---
+++ MX6WLR_0310/0310/5gyak_c_programok/mx6wlrgyak2.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void){
+
+char bekert[100];
+    printf("Enter the command:");
+    scanf("%s", read);
+
+        system(read);
+
+return 0;
+}
